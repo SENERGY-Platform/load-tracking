fix computation of last relevant timestamp

algo/utils.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import numpy as np
+
 
 def todatetime(timestamp):
         if str(timestamp).isdigit():
@@ -13,7 +13,7 @@ def todatetime(timestamp):
 def check_if_solar_power_sufficient(mean_features, solar_forecast):
     mean_length = mean_features['mean_length']
     current_time = solar_forecast[0][0]
-    last_relevant_time_of_forecast_index = np.argmin([forecast_ts for forecast_ts, _ in solar_forecast if forecast_ts>=current_time+pd.Timedelta(mean_length,'seconds')])
+    last_relevant_time_of_forecast_index = len(solar_forecast) - len([forecast_ts for forecast_ts, _ in solar_forecast if forecast_ts>=current_time+pd.Timedelta(mean_length,'seconds')])
     relevant_forecasts = [solar_forecast[i] for i in range(last_relevant_time_of_forecast_index)]
     threshold = mean_features['mean_threshold']
     if [forecast for forecast in relevant_forecasts if forecast < threshold] == []: