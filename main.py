bfb08d254743d174b792a26a684e49eaedaac405
Christoph Schrade <schrade.christoph@gmail.com>
2024-02-15 17:20:20 +0100

    use new python operator lib

--- main.py
+++ main.py
@@ -14,56 +14,91 @@
    limitations under the License.
 """
 
-import util
-import algo
-import json
-import confluent_kafka
-import mf_lib
-import cncr_wdg
-import signal
+__all__ = ("Operator", )
 
-if __name__ == '__main__':
-    util.print_init(name="anomaly-detector-operator", git_info_file="git_commit")
-    dep_config = util.DeploymentConfig()
-    opr_config = util.OperatorConfig(json.loads(dep_config.config))
-    util.init_logger(opr_config.config.logger_level)
-    util.logger.debug(f"deployment config: {dep_config}")
-    util.logger.debug(f"operator config: {opr_config}")
-    filter_handler = mf_lib.FilterHandler()
-    for it in opr_config.inputTopics:
-        filter_handler.add_filter(util.gen_filter(input_topic=it, selectors=opr_config.config.selectors, pipeline_id=dep_config.pipeline_id))
-    kafka_brokers = ",".join(util.get_kafka_brokers(zk_hosts=dep_config.zk_quorum, zk_path=dep_config.zk_brokers_path))
-    kafka_consumer_config = {
-        "metadata.broker.list": kafka_brokers,
-        "group.id": dep_config.config_application_id,
-        "auto.offset.reset": dep_config.consumer_auto_offset_reset_config,
-        "max.poll.interval.ms": 6000000
-    }
-    kafka_producer_config = {
-        "metadata.broker.list": kafka_brokers,
-    }
-    util.logger.debug(f"kafka consumer config: {kafka_consumer_config}")
-    util.logger.debug(f"kafka producer config: {kafka_producer_config}")
-    kafka_consumer = confluent_kafka.Consumer(kafka_consumer_config, logger=util.logger)
-    kafka_producer = confluent_kafka.Producer(kafka_producer_config, logger=util.logger)
-    operator = algo.Operator(
-        data_path=opr_config.config.data_path
-    )
-    operator.init(
-        kafka_consumer=kafka_consumer,
-        kafka_producer=kafka_producer,
-        filter_handler=filter_handler,
-        output_topic=dep_config.output,
-        pipeline_id=dep_config.pipeline_id,
-        operator_id=dep_config.operator_id
-    )
-    watchdog = cncr_wdg.Watchdog(
-        monitor_callables=[operator.is_alive],
-        shutdown_callables=[operator.stop],
-        join_callables=[kafka_consumer.close, kafka_producer.flush],
-        shutdown_signals=[signal.SIGTERM, signal.SIGINT, signal.SIGABRT],
-        logger=util.logger
-    )
-    watchdog.start(delay=5)
-    operator.start()
-    watchdog.join()
+from operator_lib.util import OperatorBase, Selector
+import os
+import pickle
+from algo import load_device, utils
+
+from operator_lib.util import Config
+class CustomConfig(Config):
+    data_path = "/opt/data"
+
+
+class Operator(OperatorBase):
+    configType = CustomConfig
+
+    selectors = [
+        Selector([{"name": "device_data", "args": ["time", "energy", "power"]}, 
+                  {"name": "solar_forecast", "args": ["solar_forecast", "solar_forecast_timestamp"]}])
+    ]
+
+    def init(self, *args, **kwargs):
+        super().init(*args, **kwargs)
+        data_path = self.config.data_path
+
+        if not os.path.exists(data_path):
+            os.mkdir(data_path)
+
+        self.power_list = []
+        self.energy_list = []
+
+        self.active = False
+
+        self.list_of_loads = []
+
+        self.mean_features = {'mean_consumed_energy': 0, 'mean_max_power': 0, 'mean_length': 0, 'mean_threshold': 0} # mean_length is given in seconds
+
+        self.loads_path = f'{data_path}/loads.pickle'
+        self.mean_features_path = f'{data_path}/mean_features.pickle'
+
+        self.results_from_same_weather_forecast = []
+
+        self.load_data()
+
+    def save_data(self):
+        with open(self.loads_path, 'wb') as f:
+            pickle.dump(self.list_of_loads, f)
+        with open(self.mean_features_path, 'wb') as f:
+            pickle.dump(self.mean_features, f)
+
+    def load_data(self):
+        if os.path.exists(self.loads_path):
+            with open(self.loads_path, 'rb') as f:
+                self.list_of_loads = pickle.load(f)
+        if os.path.exists(self.mean_features_path):
+            with open(self.mean_features_path, 'rb') as f:
+                self.mean_features = pickle.load(f)
+      
+    def run(self, data, selector):
+        print(f"{selector}  :   {str(data)}")
+        #print(f"{selector}  :   {str(data)}")
+        if selector == "device_data":
+            timestamp = utils.todatetime(data["time"])
+            energy = float(data["energy"])
+            power = float(data["power"])
+            self.energy_list.append([timestamp, energy])
+            self.power_list.append([timestamp, power])
+            #print('energy: '+str(energy)+'  '+'power: '+str(power)+'   '+'time: '+str(timestamp))
+            old_number_of_loads = len(self.list_of_loads)
+            self.list_of_loads, self.mean_features, self.active = load_device.online_tracking_loads(self.power_list, self.energy_list, self.list_of_loads, self.mean_features, self.active)
+            if len(self.list_of_loads) > old_number_of_loads:
+                self.save_data()
+                self.energy_list = []
+                self.power_list = []
+        elif selector == "solar_forecast":
+            if len(self.results_from_same_weather_forecast)<47:
+                self.results_from_same_weather_forecast.append(data)
+            elif len(self.results_from_same_weather_forecast)==47:
+                self.results_from_same_weather_forecast.append(data)
+                solar_forecast = [(data["solar_forecast_timestamp"], data["solar_forecast"]) for data in self.results_from_same_weather_forecast]
+                if len(self.list_of_loads) > 0:
+                    activate_device = utils.check_if_solar_power_sufficient(self.mean_features, solar_forecast)
+                    print(f"Activate Device: {activate_device}")
+                    return {'activate_device': activate_device}
+                self.results_from_same_weather_forecast = []
+
+from operator_lib.operator_lib import OperatorLib
+if __name__ == "__main__":
+    OperatorLib(Operator(), name="load-tracking-operator", git_info_file='git_commit')
